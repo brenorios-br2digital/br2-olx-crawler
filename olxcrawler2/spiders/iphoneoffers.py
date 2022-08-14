500f7cbcc88e57ade80b1fded213e60f145cde58
brenorios-br2digital <74222632+brenorios-br2digital@users.noreply.github.com>
2022-08-14 22:04:18 +0100

    Added detail page scrape

--- olxcrawler2/spiders/iphoneoffers.py
+++ olxcrawler2/spiders/iphoneoffers.py
@@ -8,8 +8,8 @@ from datetime import datetime, timedelta
 
 class IphoneoffersSpider(scrapy.Spider):
     name = 'iphoneoffers'
-    allowed_domains = ['http://www.olx.com.br/', 'www.olx.com.br']
-    start_urls = ['http://www.olx.com.br/celulares/iphone/usado?q=iphone/']
+    allowed_domains = ['http://www.olx.com.br/', 'olx.com.br']
+    start_urls = ['https://www.olx.com.br/brasil?q=iphone']
 
     def parse(self, response):
         offerItem = IphoneOfferItem()
@@ -17,7 +17,8 @@ class IphoneoffersSpider(scrapy.Spider):
         
         for offer in offerList: 
             offerItem["product_id"] = offer.xpath(XPATH_FEATURES["product_id"]).get()
-            offerItem["url"] = offer.xpath(XPATH_FEATURES["url"]).get()
+            detail_url = offer.xpath(XPATH_FEATURES["url"]).get()
+            offerItem["url"] = detail_url
             offerItem["title"] = offer.xpath(XPATH_FEATURES["title"]).get()
             offerItem["price"] = offer.xpath(XPATH_FEATURES["price"]).get()
             offerItem["post_time"] = offer.xpath(XPATH_FEATURES["post_time"]).get()
@@ -25,18 +26,44 @@ class IphoneoffersSpider(scrapy.Spider):
             offerItem["thumb_url"] = offer.xpath(XPATH_FEATURES["thumb_url"]).get()
             offerItem["is_featured"] = offer.xpath(XPATH_FEATURES["is_featured"]).get()
             offerItem["list_position"] = offer.xpath(XPATH_FEATURES["list_position"]).get()
-            
+            offerItem["vehicle_report_enabled"] = offer.xpath(XPATH_FEATURES["vehicle_report_enabled"]).get()
+            offerItem["last_bump_age_secs"] = offer.xpath(XPATH_FEATURES["last_bump_age_secs"]).get()
+            offerItem["olx_pay_enabled"] = offer.xpath(XPATH_FEATURES["olx_pay_enabled"]).get()
+            offerItem["olx_delivery_enabled"] = offer.xpath(XPATH_FEATURES["olx_delivery_enabled"]).get()
 
-            yield offerItem
+            print("___CITY___", flush=True)
+            print(offerItem["city"])
 
-        nextPageUrl = response.xpath().get(XPATH_FEATURES["next_page"])
+            if detail_url is not None:
+                print("url is not none", flush=True)
+                detail_req = scrapy.Request(url=detail_url, callback=self.parse_details)
+                detail_req.meta['item'] = offerItem
+
+                yield detail_req
+
+    def parse_details(self, response):
+        item = response.meta['item']
+        item["description"] = response.xpath(XPATH_FEATURES["description"]).get()
+        item["zipcode"] = response.xpath(XPATH_FEATURES["zipcode"]).get()
+        item["neighbourhood"] = response.xpath(XPATH_FEATURES["neighbourhood"]).get()
+        item["seller_user"] = response.xpath(XPATH_FEATURES["seller_user"]).get()
+        item["seller_join_date"] = response.xpath(XPATH_FEATURES["seller_join_date"]).get()
+        item["seller_phone_verified"] = response.xpath(XPATH_FEATURES["seller_phone_verified"]).get()
+        item["seller_email_verified"] = response.xpath(XPATH_FEATURES["seller_email_verified"]).get()
+        item["seller_facebook_verified"] = response.xpath(XPATH_FEATURES["seller_facebook_verified"]).get()
+        
+        yield item
+
+        nextPageUrl = response.xpath(XPATH_FEATURES["next_page"]).get()
         if nextPageUrl is not '' and nextPageUrl is not None:
             if 'o=' in nextPageUrl:
                 pageNum = nextPageUrl.split('o=')[1].split('&')[0]
-                print(f'Current page: {pageNum}')
+                print(f'Current page: {pageNum}', flush=True)
             else:
-                print(f'Current page: 1')
-            yield response.follow(url=nextPageUrl, callback=self.parse)   
+                print(f'Current page: 1', flush=True)
+            yield response.follow(url=nextPageUrl, callback=self.parse)  
+
+        
 
 
     
